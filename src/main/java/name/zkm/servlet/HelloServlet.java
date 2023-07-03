d9c244aadb2a68cb4c6b7c5ed20b808814661068
Administrator <Administrator@wendy-PC>
2023-07-03 21:44:15 +0800

    Servlet随着Tomcat启动；单实例多线程测试.

--- src/main/java/name/zkm/servlet/HelloServlet.java
+++ src/main/java/name/zkm/servlet/HelloServlet.java
@@ -8,16 +8,18 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 /**
- * 最基本的Servlet。
- * 1.创建一个雷，实现Servlet接口，重写必要的方法
- * 2.在web.xml文件中进行配置。
- * 3.在Tomcat中部署该Servlet，在浏览器中访问。
+ * Servlet的生命周期。
+ * 1.Servlet默认是不会随着Tomcat启动的，而是在第一次访问时创建。如果需要自动启动，需要配置
+ * 2.Servlet是单例多线程的。
  * 
  * @author Administrator
  *
  */
 public class HelloServlet implements Servlet {
 
+	// 此变量用来测试Servlet的单例多线程特性
+	private int visited = 0;
+	
 	public HelloServlet() {
 		System.out.println("调用无参构造方法");
 	}
@@ -48,7 +50,8 @@ public class HelloServlet implements Servlet {
 
 	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
-		System.out.println("service servlet");
+		visited++;
+		System.out.println("service servlet:visited:" + visited);
 
 	}
 
