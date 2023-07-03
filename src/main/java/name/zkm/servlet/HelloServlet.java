03305a47bfd9a234ac963fd087aa132172b22146
Administrator <Administrator@wendy-PC>
2023-07-03 22:02:24 +0800

    ServletConfig相关

--- src/main/java/name/zkm/servlet/HelloServlet.java
+++ src/main/java/name/zkm/servlet/HelloServlet.java
@@ -1,6 +1,8 @@
 package name.zkm.servlet;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -8,17 +10,16 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 /**
- * Servlet的生命周期。
- * 1.Servlet默认是不会随着Tomcat启动的，而是在第一次访问时创建。如果需要自动启动，需要配置
- * 2.Servlet是单例多线程的。
+ * ServletConfig:Servlet的配置。
+ * 1.Servlet的配置封装在ServletConfig中，这是个接口。封装后的对象会在init方法中传进来
+ * 2.一般会在类中写一个私有变量，用来承接ServletConfig，以便后续使用。
  * 
  * @author Administrator
  *
  */
 public class HelloServlet implements Servlet {
-
-	// 此变量用来测试Servlet的单例多线程特性
-	private int visited = 0;
+	
+	private ServletConfig config;
 	
 	public HelloServlet() {
 		System.out.println("调用无参构造方法");
@@ -45,13 +46,22 @@ public class HelloServlet implements Servlet {
 	@Override
 	public void init(ServletConfig arg0) throws ServletException {
 		System.out.println("init servlet");
-
+		this.config = arg0; // 接住ServletConfig
+		
+		System.out.println("servletName:" + config.getServletName());
+		System.out.println("servletContextPath:" + config.getServletContext().getContextPath());
+		Enumeration<String> en = config.getInitParameterNames();
+		while(en.hasMoreElements()) {
+			String paramName = en.nextElement();
+			System.out.println("paramName:" + paramName + ",paramValue:" + config.getInitParameter(paramName));
+		}
 	}
 
 	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
-		visited++;
-		System.out.println("service servlet:visited:" + visited);
+		System.out.println("service servlet" );
+		String paramValue = config.getInitParameter("param1");
+		System.out.println("paramName:param1, paramValue:" + paramValue);
 
 	}
 
